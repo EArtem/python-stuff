b5fd94c124235d69acf1b62ed3335f03edd119ce
Artem Evgrafov <artemevgrafov@Artems-MacBook-Pro.local>
2022-02-13 01:38:28 +0200

    screen update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from turtle import Screen
 from paddles import Paddle
 
-
 screen = Screen()
 screen.title('PinPong')
 screen.setup(800, 600)
 screen.bgcolor('black')
+screen.tracer(0)
 
 right_paddle = Paddle()
 right_paddle.move_paddle_to_start_position('right')
@@ -18,10 +18,11 @@ screen.onkey(right_paddle.move_down, 'Down')
 screen.onkey(left_paddle.move_up, 'a')
 screen.onkey(left_paddle.move_down, 'z')
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 screen.exitonclick()
-
-
-
 
 '''
 1. class ball
