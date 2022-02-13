bebcf87ade2eb330b779fa93daa40957af56aa7f
Artem Evgrafov <artemevgrafov@Artems-MacBook-Pro.local>
2022-02-13 15:58:53 +0200

    added scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddles import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+
 RIGHT_PADDLE_POSITION = (380, 0)
 LEFT_PADDLE_POSITION = (-390, 0)
 
@@ -9,6 +11,8 @@ screen.title('PinPong')
 screen.setup(800, 600)
 screen.bgcolor('black')
 screen.tracer(0)
+
+score = Scoreboard()
 
 right_paddle = Paddle(RIGHT_PADDLE_POSITION)
 left_paddle = Paddle(LEFT_PADDLE_POSITION)
@@ -24,33 +28,19 @@ game_is_on = True
 while game_is_on:
     screen.update()
     ball.move()
+    # top and bottom wall bounce
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-    if ball.xcor() >= (RIGHT_PADDLE_POSITION[0] - 20) and ball.distance(right_paddle.position()) < 50\
+    # collision with paddles
+    if ball.xcor() >= (RIGHT_PADDLE_POSITION[0] - 20) and ball.distance(right_paddle.position()) < 50 \
             or ball.xcor() <= (LEFT_PADDLE_POSITION[0] + 20) and ball.distance(left_paddle.position()) < 50:
         ball.bounce_x()
+    # paddle miss the ball
     if ball.xcor() > (RIGHT_PADDLE_POSITION[0] + 20):
         ball.reset_ball()
+        score.l_point()
     if ball.xcor() < (LEFT_PADDLE_POSITION[0] - 20):
         ball.reset_ball()
-
+        score.r_point()
 
 screen.exitonclick()
-
-'''
-1. class ball
-    1.1 def start game - ball starts to move to randon x that = screen x, y that == y max or y min
-    1.2 top and bottom edges should deflect the ball
-    1.3 planks should reflect the ball
-    1.4 respawn on the centre
-2. Class planks - respawn on the left and right edges (might be under the same class with ball, as table elements)
-    2.1 should have methods to move up and down;
-
-3. class scoreboard should calculate score:
-    3.1 should have left and right players
-    3.2 If ball position < x.max_left
-        right player score ++
-    3.3 If ball position > x.max_right
-        left player score ++
-
-'''
