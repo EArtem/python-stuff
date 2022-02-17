53570f2db10dd32de240a2b0b96902a2d4decb18
Artem Evgrafov <artemevgrafov@Artems-MacBook-Pro.local>
2022-02-18 00:41:47 +0200

    refactor comprehension

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ is_game_on = True
 data = pd.read_csv(ALL_STATES)
 guessed_states = []
 
+'''
+[state for state in list_of_all_states if
+                              all(guessed_state not in state for guessed_state in guessed_states)]
+'''
+
 while len(guessed_states) != 50:
     answer_state = screen.textinput(title=f'{len(guessed_states)}/50 States Correct', prompt='What another state\'s '
                                                                                              'name?').title()
     get_data = data.get(data.state == answer_state)
 
     if answer_state == 'Exit':
+        list_of_all_states = data.state.to_list()
+        not_guessed_states = [state for state in list_of_all_states if state not in guessed_states]
+        df = pd.DataFrame(not_guessed_states, columns=['state'])
+        df.to_csv('/Users/artemevgrafov/PycharmProjects/python-stuff/data/not_guessed_state.csv', index=False, mode='w')
         break
 
     if not get_data.empty and answer_state not in guessed_states:
@@ -32,10 +41,3 @@ while len(guessed_states) != 50:
         state.name_state(name_of_the_state)
         guessed_states.append(name_of_the_state[0])
         print(guessed_states)
-
-list_of_all_states = data.state.to_list()
-
-not_guessed_states = [b for b in list_of_all_states if
-                      all(a not in b for a in guessed_states)]
-df = pd.DataFrame(not_guessed_states, columns=['state'])
-df.to_csv('/Users/artemevgrafov/PycharmProjects/python-stuff/data/not_guessed_state.csv', index=False, mode='w')
